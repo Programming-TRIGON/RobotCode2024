585951ea56b0f52ccebd030d06b25029ab9cb97f
Yishai Levy <96019039+levyishai@users.noreply.github.com>
2024-01-11 23:00:09 +0200

    Moved the commands class to the commands package and added the `LinearInterpolation` class

--- src/main/java/frc/trigon/robot/commands/Commands.java
+++ src/main/java/frc/trigon/robot/commands/Commands.java
@@ -1,13 +1,20 @@
-package frc.trigon.robot.utilities;
+package frc.trigon.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.trigon.robot.constants.CommandConstants;
 import frc.trigon.robot.subsystems.MotorSubsystem;
+import frc.trigon.robot.subsystems.pitcher.Pitcher;
+import frc.trigon.robot.subsystems.pitcher.PitcherCommands;
+import frc.trigon.robot.subsystems.shooter.Shooter;
+import frc.trigon.robot.subsystems.shooter.ShooterCommands;
 import frc.trigon.robot.subsystems.swerve.Swerve;
 
 public class Commands {
+    private static final Pitcher PITCHER = Pitcher.getInstance();
+    private static final Shooter SHOOTER = Shooter.getInstance();
     private static boolean IS_BRAKING = true;
 
     /**
