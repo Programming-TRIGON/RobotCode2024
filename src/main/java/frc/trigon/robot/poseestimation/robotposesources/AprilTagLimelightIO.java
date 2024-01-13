b665fdcec3cfff9006f68b5526dc4db5f2d1ee0e
Yishai Levy <96019039+levyishai@users.noreply.github.com>
2024-01-14 00:06:39 +0200

    Cleaned up the apriltag limelight code

--- src/main/java/frc/trigon/robot/poseestimation/robotposesources/AprilTagLimelightIO.java
+++ src/main/java/frc/trigon/robot/poseestimation/robotposesources/AprilTagLimelightIO.java
@@ -1,19 +1,21 @@
 package frc.trigon.robot.poseestimation.robotposesources;
 
-import frc.trigon.robot.components.FiducialLimelight;
+import frc.trigon.robot.utilities.LimelightHelpers;
 
 public class AprilTagLimelightIO extends RobotPoseSourceIO {
-    private final FiducialLimelight fiducialLimelight;
+    private final String hostname;
 
     protected AprilTagLimelightIO(String hostname) {
-        fiducialLimelight = new FiducialLimelight(hostname);
+        this.hostname = hostname;
     }
 
     @Override
     protected void updateInputs(RobotPoseSourceInputsAutoLogged inputs) {
-        inputs.hasResult = fiducialLimelight.hasResults();
-        if (inputs.hasResult)
-            inputs.cameraPose = RobotPoseSource.pose3dToDoubleArray(fiducialLimelight.getRobotPoseFromJsonDump());
-        inputs.lastResultTimestamp = fiducialLimelight.getLastResultTimestamp();
+        inputs.hasResult = LimelightHelpers.getTV(hostname);
+        if (inputs.hasResult) {
+            final LimelightHelpers.Results results = LimelightHelpers.getLatestResults(hostname).targetingResults;
+            inputs.cameraPose = RobotPoseSource.pose3dToDoubleArray(results.getBotPose3d_wpiBlue());
+            inputs.lastResultTimestamp = results.timestamp_LIMELIGHT_publish;
+        }
     }
 }
